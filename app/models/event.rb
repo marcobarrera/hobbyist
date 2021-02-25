a6d0109db761db0731895de22beb2034b8dd26a6
Marco Barrera <marcobarrera749@gmail.com>
2021-02-25 08:22:48 -0800

    Added pg search to search form

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,4 +14,11 @@ class Event < ApplicationRecord
       errors.add(:date, "can't be in the past")
     end
   end
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_address,
+    against: [ :name, :address ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
