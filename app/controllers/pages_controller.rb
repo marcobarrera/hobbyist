1625bcef2d984f3f964b0661efec625001c9dc25
Marco Barrera <marcobarrera749@gmail.com>
2021-02-25 19:54:31 -0800

    Changed card styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @events = Event.where("date >= ?", Date.today).order(date: :asc).limit(4)
+    @events = Event.where("date >= ?", Date.today).order(date: :asc).limit(3)
   end
 
 end
