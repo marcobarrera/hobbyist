7069755470720ff9f35503b25011a86ba39722fe
Marco Barrera <marcobarrera749@gmail.com>
2021-03-01 21:03:30 -0800

    Added profile image to account avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :bookings
   has_many :events, through: :bookings
+  has_one_attached :avatar
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
