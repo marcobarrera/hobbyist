5f6e63965a22eeb29d173daee32e659425d7523c
Marco Barrera <marcobarrera749@gmail.com>
2021-03-19 16:09:45 -0700

    Fixed styling errors

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,7 +14,7 @@ class EventsController < ApplicationController
       {
         lat: event.latitude,
         lng: event.longitude,
-        infoWindow: render_to_string(partial: "info_window", locals: { event: event }),
+        infoWindow: render_to_string(partial: "info_window", locals: { event: event })
       }
     end
   end
--- app/models/event.rb
+++ app/models/event.rb
@@ -6,19 +6,17 @@ class Event < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
   validates :name, :capacity, :address, :description, :category, :date, :photo, presence: true
   validate :event_date_cannot_be_in_the_past
-  validates :category, inclusion: { in: %w(music sports poetry fitness),
-                            message: "%{value} is not valid" }
+  validates :category, inclusion: { in: %w[music sports poetry fitness],
+                                    message: "%{value} is not valid" }
 
   def event_date_cannot_be_in_the_past
-    if date.present? && date < Date.today
-      errors.add(:date, "can't be in the past")
-    end
+    errors.add(:date, "can't be in the past") if date.present? && date < Date.today
   end
 
   include PgSearch::Model
   pg_search_scope :search_by_name_and_address_and_category,
-                  against: [:name, :address, :category],
+                  against: %i[name address category],
                   using: {
-                    tsearch: { prefix: true }, # <-- now `superman batm` will return something!
+                    tsearch: { prefix: true } # <-- now `superman batm` will return something!
                   }
 end
